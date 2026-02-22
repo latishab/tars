783b5713fff32755b2e4d0ed1161079771273c67
Latisha Besariani Hendra <latishabesariani@gmail.com>
2026-02-22 17:20:41 +0800

    bump version to 0.5.2

--- tars_daemon.py
+++ tars_daemon.py
@@ -147,7 +147,7 @@ class TARSDaemon:
 
         app = FastAPI(
             title="TARS Robot API",
-            version="3.0.0",
+            version="3.1.0",
             description="""
 ## TARS Robot Control & Management API
 
