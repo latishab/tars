7322afb72165a7f73c4f17e904bedade66340053
atomikspace <skynet.terminal007@gmail.com>
2026-02-23 13:37:09 -0500

    toaster sprite path fix

--- src/modules/UI/screensavers/module_screensaver_toasters.py
+++ src/modules/UI/screensavers/module_screensaver_toasters.py
@@ -34,7 +34,7 @@ TARS_CHANCE = 0.5
 
 def _find_sprite_sheet():
     base = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(base, "assets", SPRITE_SHEET_NAME)
+    return os.path.join(base, "..", "assets", SPRITE_SHEET_NAME)
 
 
 def _load_sprites():
