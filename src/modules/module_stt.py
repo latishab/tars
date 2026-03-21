389a0ecc6f2e4bceea4fa4e0676248db8b43b7c2
atomikspace <skynet.terminal007@gmail.com>
2026-03-21 11:09:10 -0400

    play_wav output_device

--- src/modules/module_stt.py
+++ src/modules/module_stt.py
@@ -540,8 +540,10 @@ class STTManager:
 
     def play_wav(self, filename):
         try:
+            from modules.module_tts import _resolve_output_device, _output_device
+            _resolve_output_device()
             data, sr = sf.read(filename)
-            sd.play(data * 0.5, samplerate=sr)
+            sd.play(data * 0.5, samplerate=sr, device=_output_device)
             sd.wait()
         except Exception as e:
             queue_message(f"ERROR: Playing sound file failed: {e}")
